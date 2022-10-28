ae5dbd90f16eaa3ae28c3cb8e64faf78f9795bf7
ghost1157 <47149240+ghost1157@users.noreply.github.com>
2022-10-28 14:52:24 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,5 @@
+#script converts site like "xxx.com" to another format eligible for insert to resolv.conf in linux:
+#local-zone: "xxx.com." nodefault
+#local-data: "xxx.com. A 127.0.0.1"
+#This will block current domain
+#
---
+++ resolver.py
@@ -0,0 +1,12 @@
+filename = 'input/txt'
+lz = "local-zone: "
+ld = "local-data: "
+quotes = '"'
+nd = " nodefault"
+a = " A 127.0.0.1"
+with open("input.txt", "r") as nameslst:
+	with open("output.txt", "w") as readylist:
+		for line in nameslst:
+			line = line.strip()
+			line = lz + quotes + line + "." + quotes + nd + "\n" + lz + quotes + line + "." + a + quotes + "\n"
+			readylist.writelines(line)

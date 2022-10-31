186c58d8251c7b0821c12c64700779e38ff89afa
ghost1157 <47149240+ghost1157@users.noreply.github.com>
2022-10-31 15:36:01 +0200

    Update resolver.py
    syntax error resolved

--- resolver.py
+++ resolver.py
@@ -8,5 +8,5 @@ with open("input.txt", "r") as nameslst:
 	with open("output.txt", "w") as readylist:
 		for line in nameslst:
 			line = line.strip()
-			line = lz + quotes + line + "." + quotes + nd + "\n" + lz + quotes + line + "." + a + quotes + "\n"
+			line = lz + quotes + line + "." + quotes + nd + "\n" + ld + quotes + line + "." + a + quotes + "\n"
 			readylist.writelines(line)
